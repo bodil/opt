d9ccbf31103f2154a8adcaed304322bb0d253769
Bodil Stokke <17880+bodil@users.noreply.github.com>
2023-05-14 20:18:03 +0100

    Bump std version.

--- test_deps.ts
+++ test_deps.ts
@@ -1,7 +1,7 @@
-export { assertType } from "https://deno.land/std@0.186.0/testing/types.ts";
-export type { IsExact } from "https://deno.land/std@0.186.0/testing/types.ts";
+export { assertType } from "https://deno.land/std@0.187.0/testing/types.ts";
+export type { IsExact } from "https://deno.land/std@0.187.0/testing/types.ts";
 
 export {
     assertInstanceOf,
     assertStrictEquals,
-} from "https://deno.land/std@0.186.0/testing/asserts.ts";
+} from "https://deno.land/std@0.187.0/testing/asserts.ts";
