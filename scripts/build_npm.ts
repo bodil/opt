97bf19a91e924861bd322c5666a7d1a8686ca35f
Bodil Stokke <17880+bodil@users.noreply.github.com>
2023-06-17 16:48:02 +0100

    Build docs with typedoc via dnt and auto-publish to github pages.

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -1,12 +1,25 @@
 // ex. scripts/build_npm.ts
 import { build, emptyDir } from "https://deno.land/x/dnt@0.35.0/mod.ts";
 
+const version = Deno.args[0] ?? new TextDecoder().decode(
+    (await new Deno.Command("git", {
+        args: ["tag", "--sort=-version:refname"],
+    }).output()).stdout,
+).split("\n").shift();
+if (
+    version === undefined || version.trim().length < 1 ||
+    /^[0-9]+\.[0-9]+\.[0-9]+$/.exec(version) === null
+) {
+    throw new Error(`Invalid version string provided: "${version}"`);
+}
+
 await emptyDir("./npm");
 
 await build({
     scriptModule: false,
     entryPoints: ["./mod.ts"],
     outDir: "./npm",
+    packageManager: "pnpm",
     shims: {
         // see JS docs for overview and more options
         deno: "dev",
@@ -15,7 +28,7 @@ await build({
     package: {
         // package.json properties
         name: "@bodil/opt",
-        version: Deno.args[0],
+        version,
         description: "Option types for TypeScript with real gradual typing.",
         license: "MPL-2.0+",
         repository: {
@@ -32,3 +45,54 @@ await build({
         Deno.copyFileSync("README.md", "npm/README.md");
     },
 });
+
+await Deno.writeFile(
+    "./npm/tsconfig.json",
+    new TextEncoder().encode(JSON.stringify({
+        "compilerOptions": {
+            "target": "ES2022",
+            "module": "ES2022",
+            "moduleResolution": "Node",
+            "lib": ["ES2022", "DOM"],
+            "noEmit": true,
+            "strict": true,
+            "forceConsistentCasingInFileNames": true,
+            "experimentalDecorators": true,
+            "noImplicitAny": true,
+            "strictNullChecks": true,
+            "noImplicitThis": true,
+            "alwaysStrict": true,
+            "skipLibCheck": true,
+            "allowSyntheticDefaultImports": true,
+        },
+        "exclude": ["node_modules/**"],
+    })),
+);
+await Deno.writeFile(
+    "./npm/typedoc.json",
+    new TextEncoder().encode(JSON.stringify({
+        "$schema": "https://typedoc.org/schema.json",
+        "name": "opt",
+        "out": "./doc",
+        "entryPoints": [
+            "types/mod.d.ts",
+        ],
+        "excludeNotDocumented": false,
+        "excludeInternal": true,
+        // "theme": "hierarchy",
+        "includeVersion": true,
+        "plugin": ["typedoc-plugin-mdn-links"],
+    })),
+);
+await new Deno.Command("pnpm", {
+    args: ["add", "-D", "typedoc", "typedoc-plugin-mdn-links"],
+    cwd: "./npm",
+    stdout: "inherit",
+    stderr: "inherit",
+}).output();
+await new Deno.Command("pnpm", {
+    args: ["typedoc"],
+    cwd: "./npm",
+    stdout: "inherit",
+    stderr: "inherit",
+}).output();
